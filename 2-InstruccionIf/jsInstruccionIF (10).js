98a8b23ac5a56387744aeadc9236705d13b5e4a5
alumno <alumno@utnfra.com.ar>
2020-02-11 20:02:19 -0300

    asdasd

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,14 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var random;
+	var max;
+	var min;
+
+	max 11;
+	min 1;
+
+	random = Math.floor(Math.random() * (max - min)) + min;
+	console.log (random);
 
 }//FIN DE LA FUNCIÓN
