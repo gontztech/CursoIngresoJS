505606a3a16e635aff06a501fdfc98d4f98eddeb
franco lautaro <gontztech>
2020-02-15 21:36:56 -0300

    asdas

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -10,7 +10,7 @@ function mostrar()
     } else {
 
         if (edad < 13) {
-                alert ("toda mia <3 ");
+                alert ("es menor");
 
         } else {
                 alert ("Adolescente");
