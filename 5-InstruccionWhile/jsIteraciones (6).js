b218c8a4a5cbf9617d057470000a7123bc5c818a
alumno <alumno@utnfra.com.ar>
2020-02-18 19:32:02 -0300

    iteraciones6

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,14 +1,33 @@
 function mostrar()
 {
 
-	var contador=0;
-	var acumulador=0;
+	var contador = 0;
+	var acumulador = 0;
+
+	while (contador < 5) {
+		contador ++ ;
+
+		var numero = prompt ("ingrese un numero");
+
+		numero = parseInt(numero);
 
 
+
+		while (isNaN(numero )) {
+
+		numero = prompt ("Error!!! ingrese un numero") ;
+
+		numero = parseInt (numero) ;
+		}
+
+		acumulador += numero ;
+	}
+ 
+console.log (acumulador) ;
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5
 
-acumulador = acumulador + contador;
 
-}//FIN DE LA FUNCIÓN
+
+}//FIN DE LA FUNCIÓN  
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -5,6 +5,29 @@ function mostrar()
 	var acumulador=0;
 	var respuesta='si';
 
+	
+	while (contador < 5) {
+		contador ++ ;
+
+		var numero = prompt ("ingrese un numero");
+
+		numero = parseInt(numero);
+
+
+
+		while (isNaN(numero )) {
+
+		numero = prompt ("Error!!! ingrese un numero") ;
+
+		numero = parseInt (numero) ;
+		}
+
+		acumulador += numero ;
+	}
+ 
+console.log (acumulador) ;
+
+
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
