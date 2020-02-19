ac96d0d95aacd3899cfb286787432edcf1722bea
alumno <alumno@utnfra.com.ar>
2020-02-18 21:57:56 -0300

    iteraciones9

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -3,7 +3,7 @@ function mostrar()
 
 var numero = prompt("ingrese un número entre 0 y 10.");
 while (numero < 0 || numero > 9 || isNaN(numero)  ) {
-	numero=prompt("ingrese un numero correcto!!! monkaS!!!");
+	numero=prompt(" ingrese un numero correcto , " + numero + " es erroneo " );
 
 	numero = parseInt(numero);
 }
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -5,12 +5,36 @@ function mostrar()
 	// declarar variables
 	
 	var respuesta='si';
+	var maximo;
+	var minimo;
+	var primeraVez = true
 
 	while(respuesta!='no')
 	{
-		
-	
+		var numero = prompt ("ingrese un numero");
+
+		numero=parseInt(numero);
+
+		while(isNaN(numero)) {
+			numero = prompt("ingrese un numero");
+			numero = parseInt(numero);
+		}
+	if (primeraVez){
+		primeraVez= false;
+
+		maximo=numero;
+		minimo=numero;
+	}else{
+		if (numero>maximo) {
+			maximo=numero;
+		}
 	}
+
+	respuesta = prompt("desea continuar?");
+	}
+
+	document.getElementById("maximo").value=maximo;
+	document.getElementById("minimo").value=minimo;
 
 
 
