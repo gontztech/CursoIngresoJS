bb6ae88af3fadf5d716a619adbabf5b7ff035a46
franco lautaro <gontztech>
2020-02-24 18:04:49 -0300

    parciales switch

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,25 @@
 function mostrar()
 {
+    var planetas;
+
+    planetas = (prompt ("ingrese planetas")).toLowerCase();
+
+    switch (planetas) {
+        case "marte":
+        case "jupiter":
+        case "saturno":
+        case "urano":
+        case "neptuno":
+        case "pluton":
+            alert ("aca hace frio la puta que te re pario");
+            break;
+        case "tierra":
+            alert ("aca vivimos hijo de re mil");
+            break;
+        case "venus":
+        case "mercurio":
+            alert ("aca hace calor la reconcha de tu madre");
+            break;
+    }
 
 }
