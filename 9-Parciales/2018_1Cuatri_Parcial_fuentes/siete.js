0608eee6d1ec4cf07abd19fddefc2498e218bb07
franco lautaro <gontztech>
2020-03-06 16:08:00 -0300

    parcialSieteCasiFuncional

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,51 +1,32 @@
 function mostrar()
 { 
-    var respuesta;
-    var sexo;
-    var alumnos;
-    var nota;
-    var acumuladorNotas=0;
-    var acumuladorSexo=0;
-    var acumulador=0;
-    var notaMinima;
-    var sexoMinima;
-    var contadorDeVarones=0;
-    var promedio;
-    var contador=0;
+    var iteraciones=5;
+    var contadorI=0;
+    var promedio=0;
+    var contadorVaronesSeis=0;
 
-    while (acumulador <5){  
-        acumulador++;
 
-    do {
-        nota=prompt("ingrese una nota")
-        nota = parseInt(nota);
+    do {    
+        do{
+            var notas=prompt("ingrese notas");
+            notas=parseInt(notas);
+        }while (isNaN(notas) || notas < 0 || notas > 10);
 
-    }while (  nota <0 || nota > 10);
-    
-    do{
-        sexo=prompt("ingrese sex");
+        do{
+            var sexo=prompt("ingrese f o ingrese m según el sexo");
+        }while (sexo != "f" && sexo != "m" );
 
-    }while (sexo!= "f" && sexo!= "m" );
- 
+        if (sexo == "m" && notas >5)contadorVaronesSeis++;
 
-    acumuladorNotas+=nota;
+        contadorI++;
+        console.log(contadorI);
+    }while (contadorI< iteraciones);
 
-    if (contador==1){
-        notaMinima=nota;
-        sexoMinima=sexo;
+    promedio=(notas/contadorI);
 
-    }else if (nota < notaMinima){
-        notaMinima=nota;
-        sexoMinima=sexo;
-    }
-    if (sexo== "m" && nota >= 6  ){
-        contadorDeVarones++;
+    alert ( " el promedio es de  " + promedio);
+    alert (" los varones con una nota de 6 o mayor son de  "  +contadorVaronesSeis+ " niños " );
 
-    }
-    promedio = acumuladorNotas / contador;
-    alert (" el promedio es de  " +promedio );
-    alert (" la nota mas baja es de " + notaMinima + " y el sexo de esa persona es " +sexoMinima );
-    alert (" la cantidad de varones que tuvieron una nota de 6 o mas es de  " +contadorDeVarones );
-    
-}
+
+
 }
