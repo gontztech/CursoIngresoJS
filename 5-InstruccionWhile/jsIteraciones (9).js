fb2b240c1a0a534c46533ea9240bd3abe3b32ce6
franco lautaro <gontztech>
2020-03-08 01:02:59 -0300

    parcialNueveCasiTerminado

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -4,14 +4,22 @@ function mostrar()
 	
 	
 	var numAlt=0;
+	var flagNumAlt=0;
 	var numBaj=0;
+	var flagNumBaj=0;
 	var respuesta=0;
 
 	while (respuesta != "basta"){
 		respuesta=prompt("ingrese un numero - basta para parar");
 		if (parseInt(respuesta)){
-			if      (parseInt(respuesta) > numAlt || numAlt == 0 ) numAlt= parseInt(respuesta);
-			else if (parseInt(respuesta) < numBaj || numBaj == 0 ) numBaj=parseInt(respuesta);
+			if      (parseInt(respuesta) > numAlt || numAlt == 0 ){
+					flagNumAlt=1
+					numAlt= parseInt(respuesta);
+			}
+			else if (parseInt(respuesta) < numBaj || numBaj == 0 ) {
+				flagNumBaj=1;
+				numBaj=parseInt(respuesta);
+			}
 		}
 	}
 	document.getElementById("maximo").value=numAlt;
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,70 @@
 function mostrar()
 {
+    var respuesta=0;
+    var Iteraciones=0;
+    var contadorTemperaturaPar=0;
+    var contadorProductosMenosDeCeroGrados=0;
+    var acumuladorPeso=0;
+    var promedio=0;
+    var productoMasPesado;
+    var pesoMasPesado=0;
+    var pesoMasLiviano=0;
+    var flagPesado=0;
+    var flagLiviano=0;
+   
+
+    do{
+        {
+           var marca=prompt("ingrese una marca");
+        
+         }while (marca == null || marca == "");
+         do{
+            var peso=prompt("ingrese un peso del 1 al 100");
+            peso=parseInt(peso);
+            acumuladorPeso+=peso;
+            acumuladorPeso=parseInt(acumuladorPeso);
+ 
+         }while (peso < 1 || peso >100);
+         do{
+            var temperatura=prompt("ingrese temperatura de almacenamiento - de -30 a 30 grados");
+            peso=parseInt(peso);
+        }while (temperatura <-30 || temperatura > 30);
+
+        if (temperatura%2==0){
+            contadorTemperaturaPar++;
+        }else 
+        if (temperatura < -1){
+            contadorProductosMenosDeCeroGrados++;
+
+           }
+            
+            if (parseInt(peso) < pesoMasLiviano || flagLiviano==0){
+                flagLiviano=1;
+                pesoMasLiviano=parseInt(peso);
+               
+
+            }else if (parseInt(peso) > pesoMasPesado || flagPesado==0){
+                flagPesado=1;
+                productoMasPesado=marca;
+                pesoMasPesado=parseInt(peso);
+           
+            }
+             
+     respuesta=prompt("ingrese : si . si desea ingresar mas datos");
+     Iteraciones++;
+     Iteraciones=parseInt(Iteraciones);
+
+     promedio=(acumuladorPeso/Iteraciones);
+    
+
+    }while (respuesta == "si");
+
+    document.write(" la cantidad de temperaturas pares es de  " +contadorTemperaturaPar );
+    document.write (" el producto mas pesado es " +productoMasPesado+ " y su peso es de " +pesoMasPesado );
+    document.write (" el promedio del peso es de " +promedio);
+    document.write (" La cantidad de productos que se conservan a menos de 0 grados es de " +contadorProductosMenosDeCeroGrados );
+    document.write (" el peso maximo es " +pesoMasPesado+ " y el minimo es " +pesoMasLiviano);
+
+    
 
 }
